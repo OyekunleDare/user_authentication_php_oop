1238cd7c3e501b1a9b09358a5906827446f771c7
Oyekunle Dare <115362169+OyekunleDare@users.noreply.github.com>
2022-10-25 20:41:47 +0100

    Add files via upload

---
+++ classes/Dbh.php
@@ -0,0 +1,17 @@
+<?php
+
+class Dbh{		
+	protected function connect(){
+		$host = "localhost";
+		$dbname = "zuriphp";
+		$username = "root";
+		$password = "";
+		try {
+			$db = new mysqli($host, $username, $password, $dbname);
+			return $db;
+		}catch (\mysqli_sql_exception $e) {
+     		throw new \mysqli_sql_exception($e->getMessage(), $e->getCode());
+		}
+	}
+}
+?>
---
+++ classes/Route.php
@@ -0,0 +1,57 @@
+<?php
+class formController extends UserAuth{
+	public $fullname;
+	public $email;
+	public $password;
+	public $confirmPassword;
+	public $country;
+	public $gender;
+	
+	public function __construct(){
+        $this->db = new Dbh();
+    }
+	
+	public function handleForm(){
+		switch(true) {
+            case isset($_POST['register']):
+                //code to register students 
+                $this->fullname = $_POST['fullnames'];
+                $this->email = $_POST['email'];
+                $this->password = $_POST['password'];
+                $this->confirmPassword = $_POST['confirmPassword'];
+                $this->gender = $_POST['gender'];
+                $this->country = $_POST['country'];
+                $this->register($this->fullname, $this->email, $this->password, $this->confirmPassword, $this->country, $this->gender);
+                break;
+            case isset($_POST['login']):
+                ////code to login students 
+                $this->email = $_POST['email'];
+                $this->password = $_POST['password'];
+                $this->login($this->email, $this->password);
+                break;
+            case isset($_POST['logout']):
+                //code to logout students 
+                $this->email = $_POST['email'];
+                $this->logout($this->email);
+                break;
+            case isset($_POST['delete']):
+                //code to delete students 
+                $this->email = $_POST['email'];
+                $this->deleteUser($this->email);
+                break;
+            case isset($_POST['reset']):
+                //code for updating password
+                $this->email = $_POST['email'];
+                $this->password = $_POST['password'];
+                $this->updateUser($this->email, $this->password);
+                break;
+            case isset($_POST['all']):
+                //code for getting all users
+                $this->getAllUsers();
+                break;
+            default:
+                echo 'No form was submitted';
+                break;
+        }
+    }
+	}
