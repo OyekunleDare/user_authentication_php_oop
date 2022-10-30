cbd93876a48ff1d3399879e8d6e51409ce78bc74
Oyekunle Dare <115362169+OyekunleDare@users.noreply.github.com>
2022-10-30 11:23:35 +0100

    Update Dbh.php

--- classes/Dbh.php
+++ classes/Dbh.php
@@ -1,13 +1,18 @@
 <?php
 
-class Dbh{		
-	protected function connect(){
-		$host = "localhost";
-		$dbname = "zuriphp";
-		$username = "root";
-		$password = "";
+class Dbh{	
+	private $hostname;
+    	private $username;
+    	private $password;
+    	private $dbname;
+		
+		protected function connect(){
+			$this->hostname = "localhost";
+			$this->dbname = "zuriphp";
+			$this->username = "root";
+			$this->password = "";
 		try {
-			$db = new mysqli($host, $username, $password, $dbname);
+			$db = new mysqli($this->hostname, $this->username, $this->password, $this->dbname);
 			return $db;
 		}catch (mysqli_sql_exception $e) {
      			throw new mysqli_sql_exception($e->getMessage(), $e->getCode());
